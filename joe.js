0c1b6f65ea39a97011ad45b3e476d10894cab942
youssefsheriefnour <sherif.youssef222@gmail.com>
2026-07-06 15:26:33 +0300

    third commit server with msgs

--- joe.js
+++ joe.js
@@ -1,1 +1,15 @@
-console.log("Heyy kareem");
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Heyy kareeem');
+});
+
+app.get('/goat', (req, res) => {
+  res.send('CR7 is the GOAT');
+});
+
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
