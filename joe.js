2c135a71137247bf18461e19225b6de6a0c355fe
youssefsheriefnour <sherif.youssef222@gmail.com>
2026-07-06 15:52:42 +0300

    added a POST endpoint with a database

--- joe.js
+++ joe.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  res.send('Heyy kareeem');
+const names = [];
+
+app.get('/names', (req, res) => {
+  res.json(names);
 });
 
-app.get('/goat', (req, res) => {
-  res.send('CR7 is the GOAT');
+app.post('/names', (req, res) => {
+  const name = req.body.name;
+  names.push(name);
+  res.status(201).json({ message: 'Name added!', name: name });
 });
-
 
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
