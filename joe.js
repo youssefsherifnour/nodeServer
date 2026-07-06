101d6ce51d8e13cddcb0ad204f843e23daaaf095
youssefsheriefnour <sherif.youssef222@gmail.com>
2026-07-06 16:09:00 +0300

    add POST endpoint with a msgs on feature branch

--- joe.js
+++ joe.js
@@ -14,6 +14,15 @@ app.post('/names', (req, res) => {
   res.status(201).json({ message: 'Name added!', name: name });
 });
 
+
+app.get('/hello', (req, res) => {
+  res.send('Heyy kareeem');
+});
+
+app.get('/goat', (req, res) => {
+  res.send('CR7 is the GOAT');
+});
+
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
 });
